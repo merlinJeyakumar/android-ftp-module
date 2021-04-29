2b700d88754263ef8d6be926c5c6e74c53cd556a
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2021-04-30 01:01:54 +0530

    - Password Dialog Integration

---
+++ device/src/main/java/com/device/network/ftp/server/ClipBoardHelper.kt
@@ -0,0 +1,15 @@
+package com.device.network.ftp.server
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+
+
+object ClipBoardHelper {
+    fun Context.copyTextOnClipBoard(label:String="label", text:String){
+        val clipboard: ClipboardManager? = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
+        val clip: ClipData = ClipData.newPlainText(label, text)
+        clipboard?.setPrimaryClip(clip)
+    }
+}
