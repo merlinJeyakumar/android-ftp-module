736bb3689130858e3d972e26c0a3b3e4361e5318
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2021-05-04 01:05:16 +0530

    WiFi Connection Manager Added

---
+++ device/src/main/java/com/device/connection/WiFiReceiverManager.kt
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2018 ThanksMister LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.device.connection
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.*
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.text.TextUtils
+import android.util.Log
+import com.device.R
+import com.device.connection.WiFiUtility.*
+import com.support.utills.NetworkUtils.isConnectedToWifi
+import com.support.utills.text.TextUtills.trimBeginEndDoubleQuotes
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.reflect.KClass
+
+
+/*
+https://gist.github.com/JosiasSena/100de74192ca3024da8494c1ca428294
+ */
+open class WiFiReceiverManager(
+    private val context: Context
+) {
+
+    private lateinit var connectivityManager: ConnectivityManager
+    private val TAG = this::class.java.simpleName
+    private val wifiManager: WifiManager =
+        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    private val callBackList = HashMap<KClass<out Any>, NetworkCallBack>()
+
+    companion object {
+        private var INSTANCE: WiFiReceiverManager? = null
+        var disconnecting: Boolean = false
+        var networkSSID: String = ""
+        var networkPassword: String = ""
+        var previousNetworkSSID: String = ""
+        var networkId: Int = -1
+
+        @JvmStatic
+        fun getInstance(
+            applicationContext: Context
+        ): WiFiReceiverManager {
+            if (INSTANCE == null) {
+                synchronized(WiFiReceiverManager::javaClass) {
+                    INSTANCE = WiFiReceiverManager(applicationContext)
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+
+    fun init() {
+        registerYourReceiver()
+    }
+
+    @SuppressLint("WrongConstant", "NewApi")
+    private fun registerYourReceiver() {
+        connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (!context.isConnectedToWifi()) {
+            networkCallback.onLost(null)
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        } else {
+            val request: NetworkRequest = NetworkRequest.Builder()
+                .build()
+            connectivityManager.registerNetworkCallback(request, networkCallback)
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private var networkCallback: ConnectivityManager.NetworkCallback =
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network?) {
+                if (context.isConnectedToWifi()) {
+                    val currentWiFiModel = getCurrentWiFiModel()
+                    postConnectionState(
+                        WiFiConnectionStatusModel(
+                            WiFiConnectionStatus.Connected,
+                            currentWiFiModel.ipAddress
+                        )
+                    )
+                }
+            }
+
+            override fun onLost(network: Network?) {
+                postConnectionState(WiFiConnectionStatusModel(wiFiConnectionStatus = WiFiConnectionStatus.Disconnected))
+            }
+        }
+
+    fun getCurrentWiFiModel(context: Context = this.context): WiFiConnectionModel {
+        var ssid: String? = null
+        val connManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo =
+            connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+        if (networkInfo.isConnected) {
+            val connectionInfo = wifiManager.connectionInfo
+            if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.ssid)) {
+                ssid = connectionInfo.ssid
+            }
+            return WiFiConnectionModel(
+                isConnected = networkInfo.isConnected,
+                ssid = ssid?.trimBeginEndDoubleQuotes()!!,
+                ipAddress = getHotspotAddress(wifiManager),
+                networkId = connectionInfo.networkId,
+                state = if (networkInfo.isConnected) WiFiConnectionModel.CONNECTED else WiFiConnectionModel.DISCONNECTED
+            )
+        }
+        return WiFiConnectionModel(
+            state = WiFiConnectionModel.DISCONNECTED,
+            error = Throwable("wifi_connection_not_available")
+        )
+    }
+
+    fun wifiAddress1(context: Context): String? {
+        val connManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return getDefaultIpAddresses(connManager)
+    }
+
+    fun wifiAddress2(context: Context): String? {
+        return getHotspotAddress(wifiManager)
+    }
+
+    fun wifiAddress3(context: Context): String {
+        return getDefaultIpAddresses(getConnectionManager(context))
+    }
+
+    fun wifiAddress4(context: Context): String {
+        return getIpAddress(context, wifiManager)
+    }
+
+    private fun getConnectionManager(context: Context): ConnectivityManager {
+        return context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    fun postConnectionState(wiFiConnectionStatusModel: WiFiConnectionStatusModel) {
+        callBackList.forEach {
+            it.value.onNetworkConnectivityChanged(wiFiConnectionStatusModel)
+        }
+    }
+
+    open fun addCallback(clazz: KClass<out Any>, networkCallBack: NetworkCallBack) {
+        if (!callBackList.containsKey(clazz)) {
+            callBackList[clazz] = networkCallBack
+        } else {
+            Log.e(TAG, "Illegal repeated adding")
+        }
+    }
+
+    open fun removeCallBack(clazz: KClass<out Any>) {
+        if (callBackList.containsKey(clazz)) {
+            callBackList.remove(clazz)
+        }
+    }
+}
+
+open class NetworkCallBack {
+    open fun onNetworkConnectivityChanged(wiFiConnectionStatusModel: WiFiConnectionStatusModel) {
+        /*NOOP*/
+    }
+}
+
+data class WiFiConnectionStatusModel(
+    var wiFiConnectionStatus: WiFiConnectionStatus = WiFiConnectionStatus.Disconnected,
+    var ipAddress: String? = null
+)
+
+enum class WiFiConnectionStatus {
+    Connected,
+    Disconnected
+}
---
+++ device/src/main/java/com/device/connection/WiFiUtility.java
@@ -0,0 +1,172 @@
+package com.device.connection;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.DhcpInfo;
+import android.net.LinkProperties;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class WiFiUtility {
+    public static String getIpAddress(Context context, WifiManager wifiManager) {
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        return String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+    }
+
+    @SuppressLint("NewApi")
+    public static String getDefaultIpAddresses(ConnectivityManager cm) {
+        LinkProperties prop = cm.getLinkProperties(cm.getActiveNetwork());
+        return formatIpAddresses(prop);
+    }
+
+    private static String formatIpAddresses(LinkProperties prop) {
+        if (prop == null) return null;
+        Iterator<InetAddress> iter = prop.getDnsServers().iterator();
+        // If there are no entries, return null
+        if (!iter.hasNext()) return null;
+        // Concatenate all available addresses, comma separated
+        StringBuilder addresses = new StringBuilder();
+        while (iter.hasNext()) {
+            addresses.append(iter.next().getHostAddress());
+            if (iter.hasNext()) addresses.append("\n");
+        }
+        return addresses.toString();
+    }
+
+    public static String getHotspotAddress(WifiManager manager) {
+        final DhcpInfo dhcp = manager.getDhcpInfo();
+        int ipAddress = dhcp.gateway;
+        ipAddress = (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) ?
+                Integer.reverseBytes(ipAddress) : ipAddress;
+        byte[] ipAddressByte = BigInteger.valueOf(ipAddress).toByteArray();
+        try {
+            InetAddress myAddr = InetAddress.getByAddress(ipAddressByte);
+            return myAddr.getHostAddress();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            Log.e("Wifi Class", "Error getting Hotspot IP address ", e);
+        }
+        return "null";
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void setStaticIpConfiguration(WifiManager manager,
+                                                WifiConfiguration config,
+                                                InetAddress ipAddress,
+                                                int prefixLength,
+                                                InetAddress gateway,
+                                                InetAddress[] dns) throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, NoSuchFieldException, InstantiationException {
+        // First set up IpAssignment to STATIC.
+        Object ipAssignment = getEnumValue("android.net.IpConfiguration$IpAssignment", "STATIC");
+        callMethod(config, "setIpAssignment", new String[]{"android.net.IpConfiguration$IpAssignment"}, new Object[]{ipAssignment});
+
+        // Then set properties in StaticIpConfiguration.
+        Object staticIpConfig = newInstance("android.net.StaticIpConfiguration");
+        Object linkAddress = newInstance("android.net.LinkAddress", new Class<?>[]{InetAddress.class, int.class}, new Object[]{ipAddress, prefixLength});
+
+        setField(staticIpConfig, "ipAddress", linkAddress);
+        setField(staticIpConfig, "gateway", gateway);
+        getField(staticIpConfig, "dnsServers", ArrayList.class).clear();
+        for (int i = 0; i < dns.length; i++)
+            getField(staticIpConfig, "dnsServers", ArrayList.class).add(dns[i]);
+
+        callMethod(config, "setStaticIpConfiguration", new String[]{"android.net.StaticIpConfiguration"}, new Object[]{staticIpConfig});
+
+        int netId = manager.updateNetwork(config);
+        boolean result = netId != -1;
+        if (result) {
+            boolean isDisconnected = manager.disconnect();
+            boolean configSaved = manager.saveConfiguration();
+            boolean isEnabled = manager.enableNetwork(config.networkId, true);
+            boolean isReconnected = manager.reconnect();
+        }
+    }
+
+    public static WifiConfiguration getCurrentWiFiConfiguration(Context context) {
+        WifiConfiguration wifiConf = null;
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (networkInfo.isConnected()) {
+            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
+                @SuppressLint("MissingPermission") List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
+                if (configuredNetworks != null) {
+                    for (WifiConfiguration conf : configuredNetworks) {
+                        if (conf.networkId == connectionInfo.getNetworkId()) {
+                            wifiConf = conf;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return wifiConf;
+    }
+
+    private static Object newInstance(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+        return newInstance(className, new Class<?>[0], new Object[0]);
+    }
+
+    private static Object newInstance(String className, Class<?>[] parameterClasses, Object[] parameterValues) throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException {
+        Class<?> clz = Class.forName(className);
+        Constructor<?> constructor = clz.getConstructor(parameterClasses);
+        return constructor.newInstance(parameterValues);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private static Object getEnumValue(String enumClassName, String enumValue) throws ClassNotFoundException {
+        Class<Enum> enumClz = (Class<Enum>) Class.forName(enumClassName);
+        return Enum.valueOf(enumClz, enumValue);
+    }
+
+    private static void setField(Object object, String fieldName, Object value) throws IllegalAccessException, IllegalArgumentException, NoSuchFieldException {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.set(object, value);
+    }
+
+    private static <T> T getField(Object object, String fieldName, Class<T> type) throws IllegalAccessException, IllegalArgumentException, NoSuchFieldException {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        return type.cast(field.get(object));
+    }
+
+    private static void callMethod(Object object, String methodName, String[] parameterTypes, Object[] parameterValues) throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException {
+        Class<?>[] parameterClasses = new Class<?>[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++)
+            parameterClasses[i] = Class.forName(parameterTypes[i]);
+
+        Method method = object.getClass().getDeclaredMethod(methodName, parameterClasses);
+        method.invoke(object, parameterValues);
+    }
+
+    public static void updateIpConfiguration(WifiManager wifiManager,WifiConfiguration wifiConf){
+        try {
+            setStaticIpConfiguration(wifiManager, wifiConf,
+                    InetAddress.getByName("192.168.0.100"),
+                    24,
+                    InetAddress.getByName("10.0.0.2"),
+                    new InetAddress[]{InetAddress.getByName("10.0.0.3"), InetAddress.getByName("10.0.0.4")});
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
