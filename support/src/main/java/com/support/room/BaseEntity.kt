88bd47e44e42793c77614baa70f2d68e37a1aa0d
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2021-10-22 19:24:42 +0530

    migration rule

--- support/src/main/java/com/support/room/BaseEntity.kt
+++ support/src/main/java/com/support/room/BaseEntity.kt
@@ -1,14 +1,17 @@
 package com.support.room
 
-import androidx.room.*
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.TypeConverters
+import com.support.inline.orElse
 import com.support.utills.DateConverter
 import org.joda.time.DateTime
 
 
-open class BaseEntity {
+open class BaseEntity() : Parcelable {
 
     object Fields {
-        /*const val ID = "id"*/
         const val IS_DELETED = "is_deleted"
         const val IS_ACTIVE = "is_active"
         const val CREATED_AT = "created_at"
@@ -28,5 +31,33 @@ open class BaseEntity {
     @TypeConverters(DateConverter::class)
     @ColumnInfo(name = Fields.UPDATED_AT)
     var updatedAt: DateTime? = null
+
+    constructor(parcel: Parcel) : this() {
+        isActive = parcel.readByte() != 0.toByte()
+        isDeleted = parcel.readByte() != 0.toByte()
+        createdAt = DateTime(parcel.readLong())
+        updatedAt = DateTime(parcel.readLong())
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeByte(if (isActive) 1 else 0)
+        parcel.writeByte(if (isDeleted) 1 else 0)
+        parcel.writeLong(createdAt?.millis.orElse { 0L })
+        parcel.writeLong(updatedAt?.millis.orElse { 0L })
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<BaseEntity> {
+        override fun createFromParcel(parcel: Parcel): BaseEntity {
+            return BaseEntity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<BaseEntity?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
