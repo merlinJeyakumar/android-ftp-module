8fc73e2bbf71c0d2764c063e4f56efde857ce6af
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2022-05-08 11:38:11 +0530

    ftp module

--- ftp/src/main/java/com/nativedevps/ftp/client/ClientManager.kt
+++ ftp/src/main/java/com/nativedevps/ftp/client/ClientManager.kt
@@ -1,7 +1,12 @@
 package com.nativedevps.ftp.client
 
 import android.content.Context
+import org.apache.commons.net.ftp.FTPClient
 
 class ClientManager(context: Context) : IClientManager() {
+    private var ftp = FTPClient()
 
+    fun setCredentials() {
+
+    }
 }
---
+++ ftp/src/main/java/com/nativedevps/ftp/model/CredentialModel.kt
@@ -0,0 +1,41 @@
+package com.nativedevps.ftp.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class CredentialModel(
+    var address: String? = null,
+    var userName: String? = null,
+    var password: String? = null,
+    var isAuthenticated: Boolean = false,
+    var port: String? = null,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readByte() != 0.toByte(),
+        parcel.readString() ?: "")
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(address)
+        parcel.writeString(userName)
+        parcel.writeString(password)
+        parcel.writeByte(if (isAuthenticated) 1 else 0)
+        parcel.writeString(port)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CredentialModel> {
+        override fun createFromParcel(parcel: Parcel): CredentialModel {
+            return CredentialModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CredentialModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
